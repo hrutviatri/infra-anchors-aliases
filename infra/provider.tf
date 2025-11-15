bf90b2121a45217c8f6b4b7c434c7beacaec182e
Ritesh Sharma <er.ritesh2@gmail.com>
2025-11-15 09:01:28 +0530

    Ritesh Code Update

--- infra/provider.tf
+++ infra/provider.tf
@@ -9,15 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-    # subscription_id = "95a90f4b-7e15-47ba-95ab-0d52bac078e4"
+    # subscription_id = ""
   use_oidc = true
 }
 
 terraform {
-  backend "azurerm" {
-    # resource_group_name  = "ritkargs"
-    # storage_account_name = "ritkasas"
-    # container_name       = "ritkascs"
-    # key                  = "first.tfstate"
-  }
+  backend "azurerm" {}
 }
