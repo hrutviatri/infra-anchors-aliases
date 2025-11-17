c9f9cfc799df9e85af7ee7b975bbb6f47d3cd78f
Ritesh Sharma <er.ritesh2@gmail.com>
2025-11-18 00:12:30 +0530

    LB Disconnect

--- infra/main.tf
+++ infra/main.tf
@@ -53,54 +53,54 @@ module "vm_module" {
 }
 
 
-module "sql_server" {
-  depends_on  = [module.rg_module]
-  source      = "../modules/database/azurerm_mssql_server"
-  sql_servers = var.sql_servers
-}
+# module "sql_server" {
+#   depends_on  = [module.rg_module]
+#   source      = "../modules/database/azurerm_mssql_server"
+#   sql_servers = var.sql_servers
+# }
 
-module "firewall_rule" {
-  depends_on      = [module.sql_server]
-  source          = "../modules/database/azurerm_mssql_firewall_rule"
-  firewall_rules  = var.firewall_rules
-  sql_servers     = var.sql_servers
-  sql_servers_ids = module.sql_server.sql_servers_ids
-}
+# module "firewall_rule" {
+#   depends_on      = [module.sql_server]
+#   source          = "../modules/database/azurerm_mssql_firewall_rule"
+#   firewall_rules  = var.firewall_rules
+#   sql_servers     = var.sql_servers
+#   sql_servers_ids = module.sql_server.sql_servers_ids
+# }
 
-module "database" {
-  depends_on    = [module.sql_server, module.firewall_rule]
-  source        = "../modules/database/azurerm_mssql_database"
-  sql_databases = var.sql_databases
-}
+# module "database" {
+#   depends_on    = [module.sql_server, module.firewall_rule]
+#   source        = "../modules/database/azurerm_mssql_database"
+#   sql_databases = var.sql_databases
+# }
 
 
-module "loadbalancer" {
-  source        = "../modules/loadBalancer/azurerm_lb"
-  depends_on    = [module.rg_module, module.pip_module]
-  azurerm_lb_rb = var.azurerm_lb_rb
-}
+# module "loadbalancer" {
+#   source        = "../modules/loadBalancer/azurerm_lb"
+#   depends_on    = [module.rg_module, module.pip_module]
+#   azurerm_lb_rb = var.azurerm_lb_rb
+# }
 
-module "backendaddresspool" {
-  source        = "../modules/loadBalancer/azurerm_backend_address_pool"
-  depends_on    = [module.loadbalancer]
-  backend_ap_rb = var.backend_ap_rb
-}
+# module "backendaddresspool" {
+#   source        = "../modules/loadBalancer/azurerm_backend_address_pool"
+#   depends_on    = [module.loadbalancer]
+#   backend_ap_rb = var.backend_ap_rb
+# }
 
-module "nic_bp_association" {
-  source             = "../modules/loadBalancer/azurerm_nic_bp_association"
-  depends_on         = [module.backendaddresspool , module.nic_nsg_assoc_module , module.nic_module ]
-  nic_bp_association = var.nic_bp_association
-}
+# module "nic_bp_association" {
+#   source             = "../modules/loadBalancer/azurerm_nic_bp_association"
+#   depends_on         = [module.backendaddresspool , module.nic_nsg_assoc_module , module.nic_module ]
+#   nic_bp_association = var.nic_bp_association
+# }
 
-module "lb_health_probe" {
-  source     = "../modules/loadBalancer/azurerm_lb_probe"
-  depends_on = [module.nic_bp_association]
-  lb_probe   = var.lb_probe
-}
+# module "lb_health_probe" {
+#   source     = "../modules/loadBalancer/azurerm_lb_probe"
+#   depends_on = [module.nic_bp_association]
+#   lb_probe   = var.lb_probe
+# }
 
-module "lb_rule" {
-  source       = "../modules/loadBalancer/azurerm_lb_rule"
-  depends_on   = [module.lb_health_probe, module.nic_bp_association]
-  lb_rule      = var.lb_rule
-  lb_probe_ids = module.lb_health_probe.lb_probe_ids
-}
+# module "lb_rule" {
+#   source       = "../modules/loadBalancer/azurerm_lb_rule"
+#   depends_on   = [module.lb_health_probe, module.nic_bp_association]
+#   lb_rule      = var.lb_rule
+#   lb_probe_ids = module.lb_health_probe.lb_probe_ids
+# }
